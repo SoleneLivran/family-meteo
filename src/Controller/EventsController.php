ba9e6db5a0c93941aaec92a6d3bc42cfba75023d
Solène Livran <s.livran@gmail.com>
2020-11-20 23:05:52 +0100

    perf: filter relatives array to remove those with no birthdate before sorting by next birthday + comments

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -12,23 +12,30 @@ class EventsController extends AbstractController
      * @Route("/events", name="event_list")
      */
     public function eventsCalendar() {
+        // Get all relatives (people) associated with connected user in the DB (=created by this user), by user's ID.
+        // Stored in "$relatives".
         /** @var RelativeRepository $repository */
         $repository = $this->getDoctrine()->getRepository(Relative::class);
         $relatives = $repository->findAllByUser($this->getUser()->getId());
 
+        // Filter the array : remove relatives with no birthdate
+        $relatives = array_filter($relatives, function(Relative $relative) {
+            return $relative->getBirthdate() !== null;
+        });
+
+        // Sort relatives by their next birthday :
         usort($relatives, function (Relative $a, Relative $b) {
+            // compare two incoming birthdays
             $aBirthday = $a->getNextBirthday();
             $bBirthday = $b->getNextBirthday();
 
+            // don't change order if the two birthdays are the same day
             if ($aBirthday == $bBirthday) {
                 return 0;
             }
 
+            // the relative with the earliest birthday moves up in the array, the other moves down
             return ($aBirthday < $bBirthday) ? -1 : 1;
-        });
-
-        $relatives = array_filter($relatives, function(Relative $relative) {
-            return $relative->getNextBirthday() !== null;
         });
 
         return $this->render(
