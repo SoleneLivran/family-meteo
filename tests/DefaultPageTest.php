449ce5ffa91cdf0b0e2f46854641dac6ea1bdb5e
Solène Livran <s.livran@gmail.com>
2021-03-24 19:36:02 +0100

    test: default page access when connected

---
+++ tests/DefaultPageTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Repository\UserRepository;
+
+class DefaultPageTest extends WebTestCase
+{
+    // check if user can access homepage when connected
+    public function testHomepageAccessWhileLoggedIn()
+    {
+        $client = static::createClient();
+        $userRepository = static::$container->get(UserRepository::class);
+
+        // retrieve the test user
+        $testUser = $userRepository->findOneByUsername('testuser');
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        // test e.g. the profile page
+        $client->request('GET', '/');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Testuser');
+    }
+
+    // check if user if redirected to login if trying to access homepage when not connected
+}
